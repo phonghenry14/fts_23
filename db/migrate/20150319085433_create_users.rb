22c1ffa3a9811bd70c45a0760d1936116be10ba0
phonghenry14 <phonghenry14@gmail.com>
2015-03-20 14:45:50 +0700

    update-to-user

--- db/migrate/20150319085433_create_users.rb
+++ db/migrate/20150319085433_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :role, null: false, default: "normal"
 
       t.timestamps null: false
     end
