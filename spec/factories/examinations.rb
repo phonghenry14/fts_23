9ef8498981aeef35d5298aa27fce9edd696198f3
phonghenry14 <phonghenry14@gmail.com>
2015-05-15 16:37:32 +0700

    add_rspec

--- spec/factories/examinations.rb
+++ spec/factories/examinations.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
   factory :examination do
+    user_id "1"
+    course_id "1"
+    status "Ready"
+    time_submited "0"
   end
 end
---
+++ spec/factories/options.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :option do
+    question_id "1"
+    content "phong"
+    correct "true"
+  end
+end
